773eca18797fc06d24efc4d917d298a91bb7b211
DenisLuba <denislubapershins@gmail.com>
2024-01-06 00:55:26 +0300

    Test commit

--- BubleSortRange/ProgramTest.cs
+++ BubleSortRange/ProgramTest.cs
@@ -5,6 +5,8 @@ public class ProgramTest
     public static void BubbleSortRange(int[] array, int left, int right)
     {
         left = left >= 0 && left < array.Length ? left : 0;
+
+
         right = right >= 0 && right < array.Length ? right : array.Length - 1;
         for (var i = left; i <= right; i++)
             for (var j = left; j < right; j++)
@@ -21,7 +23,7 @@ public class Tests
     [TestCase(new[] { 4, 3, 2, 1 }, 0, 2, new[] { 2, 3, 4, 1 })]
     [TestCase(new[] { 4, 3, 2, 1 }, 2, 3, new[] { 4, 3, 1, 2 })]
     [TestCase(new[] { 4, 3, 2, 1 }, 0, 12, new[] { 1, 2, 3, 4 })]
-    [TestCase(new[] { 4, 3, 2, 1 }, 0, 0, new[] { 4, 3, 2, 1 })]
+    //[TestCase(new[] { 4, 3, 2, 1 }, 0, 0, new[] { 4, 3, 2, 1 })]
     public void TestBubbleSortRange(int[] array, int left, int right, int[] result)
     {
         ProgramTest.BubbleSortRange(array, left, right);
