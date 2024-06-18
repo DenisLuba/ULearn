473dc4d1ac708688ed4c10d4de5c315e96987873
DenisLuba <denislubapershins@gmail.com>
2024-06-19 00:04:14 +0300

    MyForms. Added SizeChanged and Load

--- MyForms/Program.cs
+++ MyForms/Program.cs
@@ -2,37 +2,53 @@
 
 public class MyForm : Form
 {
+    Label label;
+    TextBox textBox;
+    Button button;
+
     public MyForm()
     {
-        var label = new Label
-        {
-            Location = new Point(0, 0),
-            Size = new Size(ClientSize.Width, 30),
-            Text = "Enter a number"
-        };
 
-        var box = new TextBox
-        {
-            Location = new Point(0, label.Bottom),
-            Size = label.Size
-        };
-
-        var button = new Button
-        {
-            Location = new Point(0, box.Bottom),
-            Size = label.Size,
-            Text = "Increment!"
-        };
+        label = new Label();
+        label.Text = "Enter a number";
+        textBox = new TextBox();
+        button = new Button { Text = "Increment!" };
 
         Controls.Add(label);
-        Controls.Add(box);
+        Controls.Add(textBox);
         Controls.Add(button);
 
         button.Click += (sender, args) =>
         {
-            string text = new Regex(@"\D").Replace(box.Text, "");
-            box.Text = (int.Parse(text == "" ? "0" : text) + 1).ToString();
+            string text = new Regex(@"\D").Replace(textBox.Text, "");
+            textBox.Text = (int.Parse(text == "" ? "0" : text) + 1).ToString();
         };
+
+        // Фиксирует размер окна
+        //FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+
+        // Запрещает развернуть окно
+        //MaximizeBox = false;
+
+        // Вызывается в тот момент, когда происходит изменение окна, но не в момент загрузки окна
+        //SizeChanged += (sender, args) => { };
+
+        // Вызывается в момент загрузки окна
+        Load += (sender, args) => OnSizeChanged(EventArgs.Empty); // вызывает SizeChanged без событий
+
+        SizeChanged += new EventHandler(MyOnSizeChanged);
+    }
+
+    private void MyOnSizeChanged(object? sender, EventArgs args)
+    {
+        label.Size = new Size(ClientSize.Width, 30);
+        textBox.Size = label.Size;
+        button.Size = label.Size;
+
+        var y = ( ClientSize.Height - label.Size.Height - textBox.Size.Height - button.Size.Height ) / 2;
+        label.Location = new Point(0, y);
+        textBox.Location = new Point(0, label.Bottom);
+        button.Location = new Point(0, textBox.Bottom);
     }
 
     public static void Main()
