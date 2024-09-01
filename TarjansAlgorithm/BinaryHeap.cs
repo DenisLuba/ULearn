ec8d1e038f8326559ecbb8e33cdce97dd53d4ca3
DenisLuba <denislubapershins@gmail.com>
2024-09-01 15:20:48 +0300

    HeapifyUp

---
+++ TarjansAlgorithm/BinaryHeap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    public class BinaryHeap
+    {
+        public static void HeapifyUp(List<int> heap)
+        {
+            var itemIndex = heap.Count - 1; // этот элемент только что вставили в кучу
+
+            // поднимем его до нужного уровня в дереве
+            while (true)
+            {
+                var parentIndex = itemIndex / 2;
+                if (parentIndex <= 0 || heap[parentIndex] <= heap[itemIndex])
+                    break;
+
+                (heap[parentIndex], heap[itemIndex]) = (heap[itemIndex], heap[parentIndex]);
+                itemIndex = parentIndex;
+            }
+        }
+    }
+}
